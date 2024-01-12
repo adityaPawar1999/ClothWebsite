390318603cc307e35ad4143e2f86afc84fe49d01
aditya pawar <pawaraditya2285@gmail.com>
2024-01-12 23:18:09 +0530

    adding header

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,9 @@
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <>
+    <Header/>
     <h1>hello world</h1>
     </>
   );
---
+++ front/src/Components/Header/Header.jsx
@@ -0,0 +1,6 @@
+const Header = () =>{
+    return(<>
+    header
+    </>)
+}
+export default Header;
