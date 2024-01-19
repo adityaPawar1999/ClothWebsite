a706f4809a03291cba1e52f6b5c0f3cea00a88e8
aditya pawar <pawaraditya2285@gmail.com>
2024-01-19 23:50:25 +0530

    remove unnecessary

--- front/src/Pages/Homepage/Homepage.jsx
+++ front/src/Pages/Homepage/Homepage.jsx
@@ -5,7 +5,6 @@ const Homepage = () =>{
     return(<>
     <HomePageStyle>
             <div className="mainImage">
-                {/* https://images.unsplash.com/photo-1506620195489-7839a134b1f3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fHw%3D */}
                 <img src="https://images.unsplash.com/photo-1521429737669-ae70d53a4c95?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
                 className="headerImage"
                 alt="headerImage"
