68420e0bc6b710b3f66379997b3de76dbc179631
aditya pawar <pawaraditya2285@gmail.com>
2024-01-19 15:50:25 +0530

    send data from front

--- backend/controls/allRoutes.js
+++ backend/controls/allRoutes.js
@@ -13,15 +13,18 @@ const demo =  (req, res) => {
     res.send("deeoom doom dam doomm");
 };
 
+
 const register = async (req, res) => {
-    console.log("we enter here");
     try {
+        const { Fname , Lname , Email , Password } = req.body;
+
         const result = await user.create({
-            Fname: "aditya",
-            Lname: "pawar",
-            Email: "aditya12@gmail.com",
-            Password: "10802160"
+            Fname,
+            Lname,
+            Email,
+            Password
         });
+
         console.log("Data created successfully:", result);
         res.status(201).json(result);  // Send JSON response with the created data
     } catch (error) {
--- backend/routers/route.js
+++ backend/routers/route.js
@@ -3,7 +3,7 @@ const router = Router();  // Create an instance of the router
 const { getData, register, demo } = require('./../controls/allRoutes'); // Corrected the path
 
 router.get('/get', getData);
-router.post('/create', register);
+router.post('/register', register);
 router.get('/demo', demo);
 
 module.exports = router;  // Corrected spelling to module.exports
