2c7577e6d4b7fbfec4bafd23395af8f81c0fa3bc
aditya pawar <pawaraditya2285@gmail.com>
2024-01-27 23:35:13 +0530

    design cart  page

--- front/src/Pages/Cart/Cart.jsx
+++ front/src/Pages/Cart/Cart.jsx
@@ -1,6 +1,20 @@
-const Cart = () =>{
-    return(<>
-    cart
+import CartStyle from "./CartStyle";
+
+const Cart = () => {
+    return (<>
+        <CartStyle>
+            <div className="container">
+                <p className="userName">Welcome MR Aditya Pawar</p>
+                <div className="row header">
+                    <div className="col-lg-8">
+                        <p className="LeftTitle"> Shopping Cart</p>
+                    </div>
+                    <div className="col-lg-4">
+                        <p className="RightTitle">Items No</p>
+                    </div>
+                </div>
+            </div>
+        </CartStyle>
     </>)
 }
 export default Cart;
---
+++ front/src/Pages/Cart/CartStyle.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const CartStyle = styled.div`
+padding-top:6rem;
+.userName{
+    font-weight:500;
+    font-size:3rem;
+    color:yellowgreen;
+}
+.header{
+    font-weight:700;
+    background:gray;
+}
+.RightTitle{
+    text-align:center;
+}`
+
+export default CartStyle;
